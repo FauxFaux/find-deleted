ffb51eba11f99930d3e8eb278c128ec183683ba3
Chris West (Faux) <git@goeswhere.com>
2017-03-21 08:33:23 +0000

    don't like that class

--- find-deleted.py
+++ find-deleted.py
@@ -8,7 +8,7 @@ import stat
 import subprocess
 import sys
 
-from typing import Dict, Iterator, List, Set, Tuple
+from typing import Callable, Dict, Iterator, List, Set, Tuple
 
 Pid = int
 
@@ -73,58 +73,60 @@ def unit_names_for(pids: Iterator[Pid]) -> Dict[Pid, str]:
     return output
 
 
-class Deleted:
+class Tracker:
     def __init__(self):
         self.permission_errors = 0  # type: int
-        self.pre_filter = lambda path: not is_magic(path) and not is_tmp(path)
 
-    def load_maps(self) -> Iterator[Tuple[Pid, List[str]]]:
-        # scandir is not closeable in 3.5
-        for entry in os.scandir('/proc'):
-            if not entry.is_dir() or not entry.name.isdigit():
+
+def load_maps(tracker: Tracker) -> Iterator[Tuple[Pid, List[str]]]:
+    # scandir is not closeable in 3.5
+    for entry in os.scandir('/proc'):
+        if not entry.is_dir() or not entry.name.isdigit():
+            continue
+        try:
+            with open('/proc/{}/maps'.format(entry.name)) as f:
+                yield (int(entry.name), f.readlines())
+        except IOError as e:
+            if e is PermissionError:
+                tracker.permission_errors += 1
+            warn("reading details of pid {}: {}".format(entry.name, e))
+
+
+def pids_using_files(tracker: Tracker, pre_filter: Callable[[str], bool]) -> Dict[str, Set[Pid]]:
+    users = collections.defaultdict(set)  # type: Dict[str, Set[Pid]]
+    for (pid, lines) in load_maps(tracker):
+        for line in lines:
+            ma = MAP_REGEX.match(line)
+            if not ma:
+                warn('parse error for /proc/{}/maps: {}'.format(pid, repr(line)))
+
+            inode = int(ma.group(1))
+            if 0 == inode:
                 continue
+
+            path = ma.group(2)
+            if path.endswith(' (deleted)'):
+                path = path[0:-10]
+
+            if not pre_filter(path):
+                continue
+
             try:
-                with open('/proc/{}/maps'.format(entry.name)) as f:
-                    yield (int(entry.name), f.readlines())
+                if os.stat(path)[stat.ST_INO] != inode:
+                    users[path].add(pid)
+            except FileNotFoundError:
+                users[path].add(pid)
             except IOError as e:
                 if e is PermissionError:
-                    self.permission_errors += 1
-                warn("reading details of pid {}: {}".format(entry.name, e))
+                    tracker.permission_errors += 1
+                warn("failed to stat {} for {}: {}".format(path, pid, e))
 
-    def pids_using_files(self) -> Dict[str, Set[Pid]]:
-        users = collections.defaultdict(set)  # type: Dict[str, Set[Pid]]
-        for (pid, lines) in self.load_maps():
-            for line in lines:
-                ma = MAP_REGEX.match(line)
-                if not ma:
-                    warn('parse error for /proc/{}/maps: {}'.format(pid, repr(line)))
-
-                inode = int(ma.group(1))
-                if 0 == inode:
-                    continue
-
-                path = ma.group(2)
-                if path.endswith(' (deleted)'):
-                    path = path[0:-10]
-
-                if not self.pre_filter(path):
-                    continue
-
-                try:
-                    if os.stat(path)[stat.ST_INO] != inode:
-                        users[path].add(pid)
-                except FileNotFoundError:
-                    users[path].add(pid)
-                except IOError as e:
-                    if e is PermissionError:
-                        self.permission_errors += 1
-                    warn("failed to stat {} for {}: {}".format(path, pid, e))
-
-        return users
+    return users
 
 
 def main():
-    data = Deleted().pids_using_files()
+    tracker = Tracker()
+    data = pids_using_files(tracker, lambda path: not is_magic(path) and not is_tmp(path))
     all_pids = (pid for pids in data.values() for pid in pids)
     units = unit_names_for(all_pids)
     for (path, pids) in sorted(data.items()):
