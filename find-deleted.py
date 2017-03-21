51a5a164a22813852a0870e4b1c5d6d9fa06a2df
Chris West (Faux) <git@goeswhere.com>
2017-03-21 08:27:23 +0000

    find units for pids

--- find-deleted.py
+++ find-deleted.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import collections
+import itertools
 import os
 import re
 import stat
+import subprocess
 import sys
 
 from typing import Dict, Iterator, List, Set, Tuple
@@ -13,6 +15,8 @@ Pid = int
 MAP_REGEX = re.compile(r'^[\da-f]+-[\da-f]+ [r-][w-][x-][sp-] '
                        r'[\da-f]+ [\da-f]{2}:[\da-f]{2} '
                        r'(\d+) *(.+)( \(deleted\))?\n$')
+
+PS_REGEX = re.compile('^ *(\d+) (.*)')
 
 
 def warn(msg: str):
@@ -35,6 +39,38 @@ def is_tmp(path: str) -> bool:
             or path.startswith('/tmp')
             or path.startswith('/run')
             or path.startswith('/var/run'))
+
+
+def split_every(n, iterable):
+    """
+    https://stackoverflow.com/questions/1915170/split-a-generator-iterable-every-n-items-in-python-splitevery
+    """
+    it = iter(iterable)
+    piece = list(itertools.islice(it, n))
+    while piece:
+        yield piece
+        piece = list(itertools.islice(it, n))
+
+
+def unit_names_for(pids: Iterator[Pid]) -> Dict[Pid, str]:
+    output = dict()  # type: Dict[Pid, str]
+    max_pids_per_call = 4096 // 8 - 32
+
+    for chunk in split_every(max_pids_per_call, pids):
+        args = ['ps', '-opid=,unit=']
+        args.extend(str(pid) for pid in chunk)
+        for line in subprocess.check_output(args).decode('utf-8').split('\n'):
+            ma = PS_REGEX.match(line)
+            if not ma:
+                continue
+
+            unit = ma.group(2)
+            if '-' == unit:
+                continue
+
+            output[int(ma.group(1))] = unit
+
+    return output
 
 
 class Deleted:
@@ -88,8 +124,13 @@ class Deleted:
 
 
 def main():
-    for path in sorted(Deleted().pids_using_files().keys()):
-        print(path)
+    data = Deleted().pids_using_files()
+    all_pids = (pid for pids in data.values() for pid in pids)
+    units = unit_names_for(all_pids)
+    for (path, pids) in sorted(data.items()):
+        print(path + ':')
+        for unit in set(units.get(pid, '???') for pid in pids):
+            print(' * ' + unit)
 
 
 if '__main__' == __name__:
